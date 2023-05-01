1b65f606de9592ad533a380bdad9af3e6f57dc00
Hira <naimulrahman32@gmail.com>
2023-05-01 23:54:13 +0600

    banner part is done. the banner image is now randomly showing from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import PageNotFound from "./pages/404/pageNotFound";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import SearchResult from "./pages/search result/SearchResult";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-      <div>app</div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
